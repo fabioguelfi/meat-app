37c1a79daa3226a2dc7409332c49c53cc60b7226
fabio guelfi <fabioguelfiunix@gmail.com>
2017-11-23 13:11:32 -0200

    produtos e evento adicionar produtos

--- meat-app-starter-master/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter-master/src/app/restaurants/restaurants.service.ts
@@ -6,6 +6,7 @@ import 'rxjs/add/operator/catch'
 import { RestaurantInterface } from "app/restaurants/restaurant/restaurant.model"
 import { MEAT_API } from '../app.api'
 import {ErrorHandler} from '../app.error-handler'
+import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item.model';
 
 @Injectable()
 export class RestaurantsService {
@@ -26,6 +27,12 @@ export class RestaurantsService {
 
     reviewsOfRestaurants(id: string): Observable<any>{
         return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurants(id: string): Observable<MenuItem[]>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
         .map(response => response.json())
         .catch(ErrorHandler.handleError)
     }
